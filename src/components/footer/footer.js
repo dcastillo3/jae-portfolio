89f77d5ca8431d61eb830e5d063dde1fe0c5aa39
Daniel <dcastillo3@gmail.com>
2024-09-25 18:33:19 -0400

    chore(theme.css): Updated the design to match initial starting point based on Dribbble mockup.

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Menu } from '../menu';
 
 function Footer() {
     return (
         <div className="footer">
-            <Menu />
+            
         </div>
     );
 };
